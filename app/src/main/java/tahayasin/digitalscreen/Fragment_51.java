b3e4077873dc5c3c512a450679426edcce6724f5
Savran Taha <savrantaha@hotmail.com>
2017-11-05 02:29:27 +0100

    kine half

--- app/src/main/java/tahayasin/digitalscreen/Fragment_51.java
+++ app/src/main/java/tahayasin/digitalscreen/Fragment_51.java
@@ -6,12 +6,18 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.ImageView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 
 public class Fragment_51 extends Fragment_00 {
 
     ImageView iv_background;
+    WebView wv;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,8 @@ public class Fragment_51 extends Fragment_00 {
         View v =inflater.inflate(R.layout.fragment_51, container, false);
 
         iv_background = (ImageView) v.findViewById(R.id.fragment_0_background);
+        wv = (WebView) v.findViewById(R.id.fragment_51_wv);
+        wv.setBackgroundColor(0x00000000);
 
         return v;
     }
@@ -31,7 +39,64 @@ public class Fragment_51 extends Fragment_00 {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-
+        downloadData();
     }
+    private void downloadData(){
+        final String contentUrl = "https://kinepolis.be/nl/vandaag";
 
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String res;
+
+                try {
+                    res = new GetRequestString(contentUrl).execute().get();
+                }
+                catch (Exception e){
+                    res = "";
+                }
+
+                final String fRes = res;
+
+                if(getActivity() != null){
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            useData(fRes);
+                        }
+                    });
+                }
+            }
+        });
+        t.setPriority(Thread.MAX_PRIORITY);
+        t.start();
+    }
+    void useData(String data){
+        try {
+            Document htmlDocument = Jsoup.parse(data);
+            //Document htmlDocument = Jsoup.connect("http://darebee.com/").get(); // TODO original code
+            Element element = htmlDocument.select("#kinepolis-movie-overview-movies").first();
+
+            // replace body with selected element
+            htmlDocument.body().empty().append(element.toString());
+            String html = htmlDocument.toString();
+
+            html = html.replace("<meta charset=\"utf-8\">", "<meta charset=\"utf-8\"><base href=\"https://www.kinepolis.be\"/>");
+
+            final String fHtml = html;
+
+
+            if(getActivity() != null){
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        wv.loadData(fHtml, "text/html", "UTF-8");
+                    }
+                });
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
